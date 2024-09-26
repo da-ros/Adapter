e588a8b4b0ee6902be548ed068a52319c8e92527
Peter <peterzd11@gmail.com>
2024-09-26 01:23:33 -0400

    agregado Crossorigin a controller

--- src/main/java/com/ups/group2/adapter/controller/TemperaturaController.java
+++ src/main/java/com/ups/group2/adapter/controller/TemperaturaController.java
@@ -4,13 +4,7 @@ package com.ups.group2.adapter.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.ups.group2.adapter.model.dto.SensorTemperaturaDTO;
 import com.ups.group2.adapter.service.AdaptadorTemperatura;
@@ -18,6 +12,7 @@ import com.ups.group2.adapter.service.RegistroTemperatura;
 
 @RestController
 @RequestMapping("/temperatura")
+@CrossOrigin(origins = "*")
 public class TemperaturaController {
     
     @Autowired
